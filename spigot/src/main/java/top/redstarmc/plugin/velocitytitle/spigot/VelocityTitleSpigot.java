e26c33767e401f2c5ace4f22f0124d9e391b69af
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-06-29 15:14:45 +0800

    feat: 初始化插件构建系统

---
+++ spigot/src/main/java/top/redstarmc/plugin/velocitytitle/spigot/VelocityTitleSpigot.java
@@ -0,0 +1,9 @@
+package top.redstarmc.plugin.velocitytitle.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VelocityTitleSpigot extends JavaPlugin {
+
+
+
+}
---
+++ velocity/src/main/java/top/redstarmc/plugin/velocitytitle/velocity/VelocityTitle.java
@@ -0,0 +1,21 @@
+package top.redstarmc.plugin.velocitytitle.velocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "velocity_title",
+        name = "VelocityTitle"
+)
+public class VelocityTitle {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
