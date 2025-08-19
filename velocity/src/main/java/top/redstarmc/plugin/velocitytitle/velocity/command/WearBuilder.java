4dd321eb23021a2d740bf0203465a77be2c58c40
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-08-19 09:54:00 +0800

    feat: 穿戴命令

--- velocity/src/main/java/top/redstarmc/plugin/velocitytitle/velocity/command/WearBuilder.java
+++ velocity/src/main/java/top/redstarmc/plugin/velocitytitle/velocity/command/WearBuilder.java
@@ -5,7 +5,10 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
+import top.redstarmc.plugin.velocitytitle.velocity.database.operate.PlayerWearOperate;
 import top.redstarmc.plugin.velocitytitle.velocity.manager.ConfigManager;
+import top.redstarmc.plugin.velocitytitle.velocity.manager.EasySQLManager;
 import top.redstarmc.plugin.velocitytitle.velocity.util.TextSerializers;
 
 public class WearBuilder extends CommandBuilder{
@@ -15,14 +18,14 @@ public class WearBuilder extends CommandBuilder{
                 .requires(source -> source.hasPermission("velocitytitle.wear"))
                 .executes(context -> {
                     context.getSource().sendMessage(TextSerializers.legacyToComponent(
-                            language.getConfigToml().getString("commands.helps.create-prefix")
+                            language.getConfigToml().getString("commands.parameter-less")
                     ));
                     return Command.SINGLE_SUCCESS;
                 })
                 .then(LiteralArgumentBuilder.<CommandSource>literal("suffix")
                         .executes(context -> {
                             context.getSource().sendMessage(TextSerializers.legacyToComponent(
-                                    language.getConfigToml().getString("commands.helps.create-prefix")
+                                    language.getConfigToml().getString("commands.parameter-less")
                             ));
                             return Command.SINGLE_SUCCESS;
                         })
@@ -30,6 +33,43 @@ public class WearBuilder extends CommandBuilder{
                                 .executes(context -> {
 
                                     String name = context.getArgument("name", String.class);
+                                    String uuid;
+                                    if(context.getSource() instanceof Player player){
+                                        uuid = player.getUniqueId().toString();
+                                    }else {
+                                        context.getSource().sendMessage(TextSerializers.legacyToComponent(
+                                                language.getConfigToml().getString("commands.console")
+                                        ));
+                                        return -1;
+                                    }
+
+                                    PlayerWearOperate.updateSuffix(EasySQLManager.getSqlManager(), uuid, name);
+
+                                    return Command.SINGLE_SUCCESS;
+                                }))
+                )
+                .then(LiteralArgumentBuilder.<CommandSource>literal("prefix")
+                        .executes(context -> {
+                            context.getSource().sendMessage(TextSerializers.legacyToComponent(
+                                    language.getConfigToml().getString("commands.parameter-less")
+                            ));
+                            return Command.SINGLE_SUCCESS;
+                        })
+                        .then(BrigadierCommand.requiredArgumentBuilder("name", StringArgumentType.greedyString())
+                                .executes(context -> {
+
+                                    String name = context.getArgument("name", String.class);
+                                    String uuid;
+                                    if(context.getSource() instanceof Player player){
+                                        uuid = player.getUniqueId().toString();
+                                    }else {
+                                        context.getSource().sendMessage(TextSerializers.legacyToComponent(
+                                                language.getConfigToml().getString("commands.console")
+                                        ));
+                                        return -1;
+                                    }
+
+                                    PlayerWearOperate.updatePrefix(EasySQLManager.getSqlManager(), uuid, name);
 
                                     return Command.SINGLE_SUCCESS;
                                 }))
